1040f77790b6aaf349f0acfa57d5de42a90447e3
namsunyoung <zztjsdud00@naver.com>
2023-05-24 12:34:41 +0900

    버튼 기능추가,스타일추가

---
+++ script/main.js
@@ -0,0 +1,8 @@
+document.querySelector("#btn01").addEventListener("Click",() => {
+    alert("무등산 상세페이지로 이동합니다!");
+    location.href="https://seorak.knps.or.kr/front/portal/visit/visitCourseSubMain.do?parkId=122000&parkNavGb=park&menuNo=7020090"
+});
+document.querySelector("#btn02").addEventListener("Click",() => {
+    alert("지리산 상세페이지로 이동합니다!");
+    location.href="https://www.knps.or.kr/front/portal/visit/visitCourseSubMain.do?parkId=120100&menuNo=7020100"
+});
